155f19badf97d110c5e50e20b9fe9f88e323a92c
Paulag10 <39172820+Paulag10@users.noreply.github.com>
2019-10-30 15:46:04 -0500

    Create HomePage.php

---
+++ HomePage.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+         <link href="Bakery.css" rel="stylesheet"/>
+        <title> Home Page </title>
+    </head>
+<body>
+    
+  <h1> Paula Sweets  <img src="images/Cupcake.jpg" alt="Avatar" class="avatars">
+                </h1>
+
+                        <ul>
+                             
+                            <li><a href="index.php?action=logIn">Log In </a></li>
+                            <li><a href="index.php?action=register"> Join Us </a></p></li>
+                             <li><a href="ContactU.php">Contact Us</a></li>
+                  </ul>
+    
+    <div id ="col_1" role="main">
+        <section>
+        <!-- display a table of products -->
+        
+        <table>
+
+        <tr>
+            <td  >
+                <p >
+                    Baked goods have been around for thousands of years. 
+                    The art of baking was developed early during the Roman Empire. <br>
+                    It was a highly famous art as Roman citizens loved baked goods and demanded for them frequently for important occasions such as feasts and weddings etc. Due to the fame and desire that the art of baking received, around 300 BC, baking was introduced as an occupation and respectable profession for Romans. The bakers began to prepare bread at home in an oven, using mills to grind grain into the flour for their breads. 
+                    The oncoming demand for baked goods vigorously continued and the first bakers' guild was established in 168 BC in Rome. <br>
+                    This drastic appeal for baked goods promoted baking all throughout Europe and expanded into the eastern parts of Asia.<br>
+                    Bakers started baking breads and goods at home and selling them out on the streets. 
+                    
+                    
+                  <img src="images/history.jpg" >
+
+                </p>
+                            </td>
+            <td>
+
+                </p>
+            </td>
+        </tr>
+   
+    </table>
+
+    </section>
+<!--        <h3> <p>
+            
+                I my name is Paula Miller. I am the crate of Paula's Sweets.
+                <br>
+                I have opened my online backer for the past 2 years. We have very many different   kinda of gooodies<br>
+                 
+            </p> 
+        
+       </h2>
+        </div>-->
+    
+      <ul class="section">
+                 <li>  <img src="images/Cupcake.jpg" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake1.png" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake4.jpg" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake5.jpg" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake2.png" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake4.jpg" alt="Avatar" height="100" width="80"> </li>
+                 
+                   
+            
+            </ul>
+        
+       
+</body>
+</html>
+
