34884c1641947d7f4661aac4557e19c443888636
Paula <Paula@192.168.1.7>
2019-11-02 15:43:32 -0500

    Merge origin/master
    Conflicts:
    	index.php

---
+++ Admin_view.php
@@ -0,0 +1,36 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+         <link href="Bakery.css" rel="stylesheet"/>
+        <title> Home Page </title>
+    </head>
+<body>
+    
+  <h1> Paula Sweets  <img src="images/Cupcake.jpg" alt="Avatar" class="avatars">
+                </h1>
+
+                        <ul>
+                             
+                            <li><a href="index.php?action=logIn">Log Out</a></li>
+                            <a href="index.php?action=viewAllUsers">View all users</a>
+                             <li><a href="ContactU.php">Contact Us</a></li>
+                  </ul>
+  
+        
+   
+    
+      <ul class="section">
+                 <li>  <img src="images/Cupcake.jpg" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake1.png" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake4.jpg" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake5.jpg" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake2.png" alt="Avatar" height="100" width="80"> </li>
+                 <li>  <img src="images/Cupcake4.jpg" alt="Avatar" height="100" width="80"> </li>
+                 
+                   
+            
+            </ul>
+        
+       
+</body>
+</html>
