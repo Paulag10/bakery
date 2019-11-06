b342a835826695e56ab555d522a4ff3d1aecff05
Paula <Paula@192.168.1.7>
2019-11-06 16:38:20 -0600

    Merge origin/master
    Conflicts:
    	model/providerdb.php

--- HomePage.php
+++ HomePage.php
@@ -1,66 +1,51 @@
+
 <!DOCTYPE html>
-
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-         <link href="Bakery.css" rel="stylesheet"/>
-        <title> Home Page </title>
-    </head>
+<head>
+  <title>Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="Bakery.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
 <body>
-    
-  <h1> Paula Sweets  <img src="images/Cupcake.jpg" alt="Avatar" class="avatars">
-                </h1>
 
-                        <ul>
+<div class="jumbotron text-center">
+ <h1> Paula Sweets  <img src="images/Cupcake.jpg" alt="Avatar" class="avatars">
+                </h1>
+</div>
+     <ul>
                              
                             <li><a href="index.php?action=logIn">Log In </a></li>
                             <li><a href="index.php?action=register"> Join Us </a></p></li>
                              <li><a href="ContactU.php">Contact Us</a></li>
                   </ul>
-    
-    <div id ="col_1" role="main">
-        <section>
-     <table>
-
-        <tr>
-            <td  >
-                <p >
-                    Baked goods have been around for thousands of years. 
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>History</h3>
+      <p>Baked goods have been around for thousands of years. 
                     The art of baking was developed early during the Roman Empire. <br>
-                    It was a highly famous art as Roman citizens loved baked goods and demanded for them frequently for important occasions such as feasts and weddings etc. Due to the fame and desire that the art of baking received, around 300 BC, baking was introduced as an occupation and respectable profession for Romans. The bakers began to prepare bread at home in an oven, using mills to grind grain into the flour for their breads. 
-                    The oncoming demand for baked goods vigorously continued and the first bakers' guild was established in 168 BC in Rome. <br>
-                    This drastic appeal for baked goods promoted baking all throughout Europe and expanded into the eastern parts of Asia.<br>
-                    Bakers started baking breads and goods at home and selling them out on the streets. 
-                    
-                    
-                  <img src="images/history.jpg" >
+                    It was a highly famous art as Roman citizens loved baked goods and demanded for them frequently for important occasions such as feasts and weddings etc. Due to the fame and desire that the art of baking received, around 300 BC, baking was introduced as an occupation and respectable profession for Romans. </p>
+      
+    </div>
+    <div class="col-sm-4">
+      
+      <p>The bakers began to prepare bread at home in an oven, using mills to grind grain into the flour for their breads. 
+                    The oncoming demand for baked goods vigorously continued and the first bakers' guild was established in 168 BC in Rome.This drastic appeal for baked goods promoted baking all throughout Europe and expanded into the eastern parts of Asia.<br>
+                    Bakers started baking breads and goods at home and selling them out on the streets. </p>
+     
+    </div>
+    <div class="col-sm-4">
+      <img src="images/history.jpg" >      
+     
+    </div>
+  </div>
+</div>
 
-                </p>
-                            </td>
-            <td>
-
-                </p>
-            </td>
-        </tr>
-   
-    </table>
-
-    </section>
-
-    
-      <ul class="section">
-                 <li>  <img src="images/Cupcake.jpg" alt="Avatar" height="100" width="80"> </li>
-                 <li>  <img src="images/Cupcake1.png" alt="Avatar" height="100" width="80"> </li>
-                 <li>  <img src="images/Cupcake4.jpg" alt="Avatar" height="100" width="80"> </li>
-                 <li>  <img src="images/Cupcake5.jpg" alt="Avatar" height="100" width="80"> </li>
-                 <li>  <img src="images/Cupcake2.png" alt="Avatar" height="100" width="80"> </li>
-                 <li>  <img src="images/Cupcake4.jpg" alt="Avatar" height="100" width="80"> </li>
-                 
-                   
-            
-            </ul>
-        
-       
 </body>
 </html>
-
